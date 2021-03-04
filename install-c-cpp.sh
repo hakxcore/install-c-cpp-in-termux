2ce152215dae3bf191546064ff42eebb5d65771c
MUKESHKUMAR2001 <mukeshkumarckarak@gmail.com>
2021-03-04 12:39:08 +0530

    installation file uploaded

---
+++ install-c-cpp.sh
@@ -0,0 +1,9 @@
+apt update && apt upgrade -y
+apt install clang
+echo "-------------------------------------------------"
+echo "      C and C++ is successfully Installed"
+echo "-------------------------------------------------"
+apt install python python3
+echo "-------------------------------------------------"
+echo "  Python and Python3 is successfully Installed"
+echo "-------------------------------------------------"
